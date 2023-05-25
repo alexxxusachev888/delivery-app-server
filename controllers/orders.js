dcece24d7d0f65a1adc33e146c899c8571c81575
alexxx888 <alexxxusachev888@gmail.com>
2023-05-25 20:35:06 +0300

    getAllOrdersByPhoneOrEmail fix

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,28 +2,21 @@ const { Order } = require('../models/order');
 const { controlWrapper, HttpError } = require('../helpers');
 
 const getAllOrdersByPhoneOrEmail = async (req, res) => {
-    const { phone, email } = req.query;
-    let orders;
+    const { searchQuery } = req.query;
 
-    if (!phone && !email) {
+    if (!searchQuery) {
         throw HttpError(400, 'Phone or email is required');
     }
-    if (phone && email) {
-        throw HttpError(400, 'Phone or email is required, not both');
-    }
     
-    if (phone) {
-        orders = await Order.find({ phone });
-        if (orders.length === 0) {
-            throw HttpError(404, 'Order not found');
-        }
-    }
-    
-    if (email) {
-        orders = await Order.find({ email });
-        if (orders.length === 0) {
-            throw HttpError(404, 'Order not found');
-        }
+    const orders = await Order.find({
+        $or: [
+            { email: searchQuery },
+            { phone: searchQuery },
+        ]
+    });
+
+    if (orders.length === 0) {
+        throw HttpError(404, 'Order not found');
     }
     
     res.json({ orders });
